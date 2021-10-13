ae620890baa2f57a3726cdc26a90299807d00621
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-10-13 23:18:57 +0100

    last commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
 const myBirthday = "22 Oct 2021";
 
 function countdown() {
@@ -8,10 +13,17 @@ function countdown() {
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = math.floor(totalSeconds) % 60;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours, minutes, seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 }
 
 //initial call
